f37690b14cd8d008dda3555e0c7d26bdb1439c05
Ryan Cragun <ryan@rightscale.com>
2011-03-09 17:39:10 -0800

    block is working, logic for passing threshold still off

--- terminator2.rb
+++ terminator2.rb
@@ -6,22 +6,28 @@ require 'time'
 
 terminate_after_hours = 24
 protection_word = "save"
-
-threshold = Time.now - (terminate_after_hours * (60 * 60))
-warning = threshold - ( 3 * (60 * 60))
+debug = 'true'
 
 @servers = Server.find_all.select { |x| x.state != "stopped" }
 @servers.each do |svr|
   unless svr.nickname.downcase.include?(protection_word)
-    settings = svr.settings_current
-    unless settings['locked'] == "true"
+    settings = svr.settings
+    unless svr.settings['locked'].to_s == "true"
       last_updated_time = Time.parse(settings['updated_at'].to_s)
-      if last_updated_time > warning && last_updated_time < threhold
-        #warn peep     
-      elsif last_updated_time < threshold
-          puts "Terminating => #{svr.nickname}"
-          #svr.stop
-          `mail -s "#{svr.nickname} has been destroyed by the Terminator." services@rightscale.com`
+      life_time = last_updated_time + (terminate_after_hours * 60 * 60)
+      warning = life_time - ( 3 * (60 * 60)) #warns 3 hours before termination
+      puts "nickname: #{settings['nickname']}\n
+       locked: #{settings['locked']}\n
+       last updated: #{last_updated_time}\n
+       life time allowed: #{life_time}\n
+       warning time: #{warning}\n" unless debug == 'false'
+      if last_updated_time > warning && last_updated_time < life_time
+        #warn peep
+        puts "Warning owner of => #{svr.nickname}\n"     
+      elsif last_updated_time > life_time
+        puts "Terminating => #{svr.nickname}\n"
+        #svr.stop
+        #`mail -s "#{svr.nickname} has been destroyed by the Terminator." services@rightscale.com`
       end
     end
   end
