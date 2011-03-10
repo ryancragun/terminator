af8b5e531ba881137c07ec29b5f00a743316f52d
Ryan Cragun <ryan@rightscale.com>
2011-03-09 17:52:19 -0800

    logic is working, just need to add mailing logic

--- terminator2.rb
+++ terminator2.rb
@@ -7,6 +7,7 @@ require 'time'
 terminate_after_hours = 24
 protection_word = "save"
 debug = 'true'
+threshold = Time.now
 
 @servers = Server.find_all.select { |x| x.state != "stopped" }
 @servers.each do |svr|
@@ -16,15 +17,15 @@ debug = 'true'
       last_updated_time = Time.parse(settings['updated_at'].to_s)
       life_time = last_updated_time + (terminate_after_hours * 60 * 60)
       warning = life_time - ( 3 * (60 * 60)) #warns 3 hours before termination
-      puts "nickname: #{settings['nickname']}\n
-       locked: #{settings['locked']}\n
-       last updated: #{last_updated_time}\n
-       life time allowed: #{life_time}\n
+      puts "nickname: #{settings['nickname']}
+       locked: #{settings['locked']}
+       last updated: #{last_updated_time}
+       life time allowed: #{life_time}
        warning time: #{warning}\n" unless debug == 'false'
-      if last_updated_time > warning && last_updated_time < life_time
+      if (threshold < warning) && (threshold < life_time)
         #warn peep
         puts "Warning owner of => #{svr.nickname}\n"     
-      elsif last_updated_time > life_time
+      elsif (threshold > life_time)
         puts "Terminating => #{svr.nickname}\n"
         #svr.stop
         #`mail -s "#{svr.nickname} has been destroyed by the Terminator." services@rightscale.com`
