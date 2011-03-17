69100c47a80110df2b588bb931d55ea2a4c8ddc6
Ryan Cragun <ryan@rightscale.com>
2011-03-17 12:44:11 -0700

    hopefully the damn email is really fixed this time

--- terminator2.rb
+++ terminator2.rb
@@ -60,7 +60,7 @@ usage if !terminate_after_hours
       elsif (current_time > life_time)
         puts "Terminating => #{svr.nickname}\n"
         svr.stop
-        `echo 'Be sure to lock or put "save" somewhere in the nickname to prevent this from happening again.' | mail -s "#{svr.nickname} has been destroyed by the Terminator." services@rightscale.com`
+        `echo "Be sure to lock or put 'save' somewhere in the nickname to prevent this from happening again." | mail -s "#{svr.nickname} has been destroyed by the Terminator." services@rightscale.com`
       end
     end
   end
